6d489e147c9769aa82ef4bb41f61635383569022
brahimjarallah <brahim.jarallah@gmail.com>
2021-06-16 18:51:59 +0100

    random-quote-machine-colorized

--- src/App.js
+++ src/App.js
@@ -16,74 +16,31 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      quotes: [
-        {
-          quote: "note quote yet",
-          author: "no  author yet",
-        },
-      ],
-      selectedQuoteIndex: null,
-      selectedColor: [
-        "#16a085",
-        "#27ae60",
-        "#2c3e50",
-        "#f39c12",
-        "#e74c3c",
-        "#9b59b6",
-        "#FB6964",
-        "#342224",
-        "#472E32",
-        "#BDBB99",
-        "#77B1A9",
-        "#73A857",
-      ],
+      color: "orange",
     }
-    this.assignNewQuoteIndex = this.assignNewQuoteIndex.bind(this)
-    this.selectQuoteIndex = this.selectQuoteIndex.bind(this)
+
+    this.handleColorChange = this.handleColorChange.bind(this)
+  }
+  handleColorChange(color) {
+    this.setState({
+      color: color,
+    })
   }
 
-  componentDidMount() {
-    fetch(
-      "https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json"
-    )
-      .then((quotes) => quotes.json())
-      .then((quotes) => {
-        this.setState({ quotes }, this.assignNewQuoteIndex)
-      })
-  }
-
-  selectQuoteIndex() {
-    if (!this.state.quotes.length) {
-      return
-    }
-    return random(0, this.state.quotes.length - 1)
-  }
-
-  get selectedQuote() {
-    if (
-      !this.state.quotes.length ||
-      !Number.isInteger(this.state.selectedQuoteIndex)
-    ) {
-      return undefined
-    }
-    return this.state.quotes[this.state.selectedQuoteIndex]
-  }
-  assignNewQuoteIndex() {
-    this.setState({ selectedQuoteIndex: this.selectQuoteIndex() })
-  }
   render() {
     return (
       <Grid
+        style={{ backgroundColor: this.state.color }}
         className={this.props.classes.container}
         id="quote-box"
         justify="center"
         container
       >
-        <Grid xs='11' lg='8' item>
-          {this.selectedQuote ? <QuoteMachine
-            selectedQuote={this.selectedQuote}
-            assignNewQuoteIndex={this.assignNewQuoteIndex}
-          /> : null}
+        <Grid xs="11" lg="8" item>
+          <QuoteMachine
+            color={this.state.color}
+            onChangeAppColor={this.handleColorChange}
+          />
         </Grid>
       </Grid>
     )
--- src/components/QuoteMachine.js
+++ src/components/QuoteMachine.js
@@ -1,7 +1,8 @@
+import React, { Component } from "react"
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faTwitter } from "@fortawesome/free-brands-svg-icons"
 
-import React from "react"
 import {
   Typography,
   Card,
@@ -11,31 +12,85 @@ import {
   IconButton,
 } from "@material-ui/core"
 
-const QuoteMachine = ({ selectedQuote, assignNewQuoteIndex }) => {
-  return (
-    <Card>
-      <CardContent>
-        {selectedQuote ? (
+class QuoteMachine extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      quote: "",
+      author: "",
+      color: this.props.color,
+      tweetUrl: "https://twitter.com/intent/tweet?text=",
+    }
+
+    this.getNewQuote()
+    this.getNewQuote = this.getNewQuote.bind(this)
+    this.handleNewQuote = this.handleNewQuote.bind(this)
+  }
+
+  getNewQuote() {
+    // const app = this
+    fetch("https://type.fit/api/quotes")
+      .then((response) => response.json())
+      .then((data) => {
+        const index = Math.floor(Math.random() * data.length)
+        this.setState({
+          quote: data[index].text,
+          author: data[index].author,
+          tweetUrl:
+            "https://twitter.com/intent/tweet/?text=" +
+            data[index].text.replace(/ /g, "+") + ' -' + data[index].author,
+        })
+      })
+  }
+
+  handleNewQuote() {
+    const colors = [
+      "#16a085",
+      "#27ae60",
+      "#2c3e50",
+      "#f39c12",
+      "#e74c3c",
+      "#9b59b6",
+      "#FB6964",
+      "#342224",
+      "#472E32",
+      "#BDBB99",
+      "#77B1A9",
+      "#73A857",
+    ]
+    let color = colors[Math.floor(Math.random() * colors.length)]
+    while (color == this.state.color)
+      if (this.state.quotes.length > 0) {
+        this.setState({
+          color: color,
+        })
+      }
+    this.props.onChangeAppColor(color)
+    this.getNewQuote()
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardContent>
+          {/* {selectedQuote ? ( */}
           <Typography>
-            {selectedQuote.quote} - {selectedQuote.author}
+            {this.state.quote} - {this.state.author}
           </Typography>
-        ) : null}
-      </CardContent>
-      <CardActions>
-        <Button size="small" onClick={assignNewQuoteIndex}>
-          Next Quote
-        </Button>
-        <IconButton
-          href={encodeURI(
-            `https://twitter.com/intent/tweet?text=${selectedQuote.quote}&hashtags=${selectedQuote.author}`
-          )}
-          target="_blank"
-        >
-          <FontAwesomeIcon icon={faTwitter} size="md"></FontAwesomeIcon>
-        </IconButton>
-      </CardActions>
-    </Card>
-  )
+          {/* )  */}
+          {/* : null} */}
+        </CardContent>
+        <CardActions>
+          <Button size="small" onClick={this.handleNewQuote}>
+            Next Quote
+          </Button>
+          <IconButton href={this.state.tweetUrl} target="_blank">
+            <FontAwesomeIcon icon={faTwitter} size="md"></FontAwesomeIcon>
+          </IconButton>
+        </CardActions>
+      </Card>
+    )
+  }
 }
 
 export default QuoteMachine
